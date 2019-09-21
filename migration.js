5c3fda88334f4d4baf38ec63103c7176405ea38c
xxxya <moon.threesix@hotmail.com>
2019-09-21 15:58:04 +0800

    Add new field for storing current index.

--- migration.js
+++ migration.js
@@ -1,21 +1,26 @@
 const defaultPreference = {
-  version: 3,
-  mode: 'day',
-  list: [{
-    content: ''
-  }]
+    version: 4,
+    mode: 'day',
+    lastIndex: 0,
+    list: [{
+        content: ''
+    }]
 }
 
-;(() => {
-  window.migration = async results => {
-    if (results.version === 2) {
-      results.list = [{
-        content: results.content || ''
-      }]
-      results.version = defaultPreference.version
-      delete results.content
-      await browser.storage.sync.set(results)
-      browser.storage.sync.remove('content')
-    }
-  }
-})()
+    ; (() => {
+        window.migration = async results => {
+            if (results.version === 2) {
+                results.list = [{
+                    content: results.content || ''
+                }]
+                results.version = defaultPreference.version
+                delete results.content
+                await browser.storage.sync.set(results)
+                browser.storage.sync.remove('content')
+            }
+            if (results.version === 3) {
+                results.lastIndex = 0
+                await browser.storage.sync.set(results)
+            }
+        }
+    })()
